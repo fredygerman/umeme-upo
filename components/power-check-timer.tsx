46be952e2524ad70d6179ef8e14e0f53bd4b50d4
Fredy <fredgerman30@gmail.com>
2023-10-29 17:50:14 +0300

    feat : added power check timer component

---
+++ components/power-check-timer.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
+
+export default function PowerCheckTimer() {
+  return <h3 className='mb-8 text-center text-2xl font-bold'>00:32:10</h3>;
+}
