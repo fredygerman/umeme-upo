d852e38d67148c9e66d1d51f345c520655aa1123
Fredy <fredgerman30@gmail.com>
2023-10-29 17:49:26 +0300

    feat : language changer

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
-import { ModeToggle } from '../misc/theme-toggle';
+import { ModeToggle } from '@/components/misc/theme-toggle';
+import { LocationDropDown } from '@/components/misc/location-dropdown';
+import { Icons } from '@/components/icons';
+import { TranslateToggle } from '@/components/misc/translate-dropdown';
 
 export function AppHeader() {
   return (
@@ -12,14 +15,13 @@ export function AppHeader() {
           height={20}
         />
       </div>
-      <div className='flex flex-col items-center justify-between'>
-        <h1 className='text-2xl font-bold'>Makumbusho</h1>
+      <div className='flex flex-col items-center justify-between '>
+        <LocationDropDown />
       </div>
-      <div className='flex flex-col items-center justify-between'>
-        <div className='flex flex-row items-center justify-between'>
-          <p className='pr-4 text-sm font-bold'>Translate</p>
-          <ModeToggle />
-        </div>
+
+      <div className='flex flex-row items-center justify-between'>
+        <TranslateToggle />
+        <ModeToggle />
       </div>
     </div>
   );
---
+++ components/misc/translate-dropdown.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Icons } from '../icons';
+import { useCallback, useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
+import { Language } from '@/types/general';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+
+export function TranslateToggle() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [language, setLanguage] = useState<Language>(
+    (searchParams.get('language') as Language) || 'en'
+  );
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  useEffect(() => {
+    router.push(pathname + '?' + createQueryString('language', language));
+  }, []);
+
+  useEffect(() => {
+    // when language changes, refresh the page
+    router.refresh();
+  }, [language]);
+
+  return (
+    <div className='pr-4'>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost'>
+            <Icons.translate
+              className={cn(
+                'mr-2 h-[1.4rem] w-[1.2rem] rotate-0 scale-100 uppercase transition-all '
+              )}
+            />
+
+            {language === 'en' ? 'English' : 'Swahili'}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56'>
+          <DropdownMenuLabel>
+            {language === 'en' ? 'Badili Lugha' : 'Change Language'}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup
+            value={language}
+            onValueChange={(value: Language | string) => {
+              setLanguage(value as Language);
+              router.push(
+                pathname + '?' + createQueryString('language', value)
+              );
+            }}
+          >
+            <DropdownMenuRadioItem value='en'>English</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value='sw'>Kiswahili</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
