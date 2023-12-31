48330ac912203d7ff6dba9294d0729fa16d0186e
Fredy <fredgerman30@gmail.com>
2023-12-31 13:45:56 +0300

    feat : disabled analytics on non production deployments

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter, Roboto } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { AppHeader } from '@/components/layout/AppHeader';
-import { useParams } from 'next/navigation';
-import { Language } from '@/types/general';
 import QueryProvider from '@/components/query-provider';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -24,11 +22,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <script
-        async
-        src='https://analytics.eu.umami.is/script.js'
-        data-website-id='52ffbed5-9bda-487a-9f25-02ab08228eb5'
-      />
+      {process.env.NODE_ENV === 'production' && (
+        <script
+          async
+          src='https://analytics.eu.umami.is/script.js'
+          data-website-id='52ffbed5-9bda-487a-9f25-02ab08228eb5'
+        />
+      )}
+
       <body className={roboto.className}>
         <ThemeProvider
           attribute='class'
