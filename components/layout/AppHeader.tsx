c699078684c41400e85131c39e43e6a6bef59330
Fredy <fredgerman30@gmail.com>
2024-02-11 19:39:31 +0300

    fix : minor style error

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -10,7 +10,7 @@ export async function AppHeader() {
   const locations = await fetchLocations();
 
   return (
-    <div className='flex flex-row items-center justify-between w-full px-8 py-2 md:px-16'>
+    <div className='flex w-full flex-row items-center justify-between px-8 py-2 md:px-16'>
       <div className='flex flex-col items-center justify-between pr-16'>
         <Image
           src='/images/logo/brand/main.svg'
